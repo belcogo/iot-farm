fb38f96d02a63d102655db70f4d92571a2cabddb
Bruno Hoffmann <bruno.siqueira.hoffmann@gmail.com>
2025-03-26 23:56:37 -0300

    fix: arruma formatação de datas

--- src/front-end/main.py
+++ src/front-end/main.py
@@ -10,7 +10,9 @@ db = mongo_client["iot_farm"]
 def fetch_data(collection_name):
     collection = db[collection_name]
     data = list(collection.find({}, {"_id": 0}))  # Exclui o campo _id
-    return pd.DataFrame(data)
+    data_frame = pd.DataFrame(data)
+    data_frame = data_frame.sort_values("created", ascending=False)
+    return data_frame
 
 # Configuração do título do app
 st.title("IoT Farm Dashboard")
@@ -19,28 +21,28 @@ st.title("IoT Farm Dashboard")
 st.header("Temperatura")
 temperature_data = fetch_data("temperature")
 temperature_data["created"] = pd.to_datetime(temperature_data["created"])
-temperature_data["created"] = temperature_data["created"].dt.strftime("%H:%M:%S")
+temperature_data["created_formatted"] = temperature_data["created"].dt.strftime("%H:%M:%S")
 st.dataframe(temperature_data)
 
 # Exibir dados de umidade
 st.header("Umidade")
 humidity = fetch_data("humidity")
 humidity["created"] = pd.to_datetime(humidity["created"])
-humidity["created"] = humidity["created"].dt.strftime("%H:%M:%S")
+humidity["created_formatted"] = humidity["created"].dt.strftime("%H:%M:%S")
 st.dataframe(humidity)
 
 # Exibir dados de luminosidade
 st.header("Luminosidade")
 brightness_data = fetch_data("brightness")
 brightness_data["created"] = pd.to_datetime(brightness_data["created"])
-brightness_data["created"] = brightness_data["created"].dt.strftime("%H:%M:%S")
+brightness_data["created_formatted"] = brightness_data["created"].dt.strftime("%H:%M:%S")
 st.dataframe(brightness_data)
 
 # Gráficos
 st.subheader("Gráficos")
 if not temperature_data.empty:
-    st.line_chart(temperature_data.set_index("created")["temperature"], use_container_width=True)
+    st.line_chart(temperature_data.set_index("created_formatted")["temperature"], use_container_width=True)
 if not humidity.empty:
-    st.line_chart(humidity.set_index("created")["humidity"], use_container_width=True)
+    st.line_chart(humidity.set_index("created_formatted")["humidity"], use_container_width=True)
 if not brightness_data.empty:
-    st.line_chart(brightness_data.set_index("created")["brightness"], use_container_width=True)
+    st.line_chart(brightness_data.set_index("created_formatted")["brightness"], use_container_width=True)
